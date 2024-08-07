8ad1239e8634bcedd859742ef62801ee37537ffb
Giovanni-Damico98 <gd.installazioni25@gmail.com>
2024-08-08 00:49:25 +0200

    js complete

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
+// Recupero gli elementi dall'html
 const inputName = document.getElementById("nameField");
 const inputKm = document.getElementById("kmField");
 const inputAge = document.getElementById("ageField");
 const button = document.getElementById("calculate");
+const clearButton = document.getElementById("clearButton");
 const price = document.getElementById("price");
 const passengerName = document.getElementById("passengerName");
 const priceForKm = 0.21;
@@ -15,7 +17,17 @@ button.addEventListener("click", function (e) {
     alert("Per favore inserisci un nome valido.");
   } else {
     let ticketPrice = inputKm.value * priceForKm;
+    if (inputAge.value === "minorenne") {
+      ticketPrice -= ticketPrice * 0.2;
+    } else if (inputAge.value === "senior") {
+      ticketPrice -= ticketPrice * 0.4;
+    }
     passengerName.innerText = inputName.value.trim();
     price.innerText = ticketPrice.toFixed(2) + " €";
   }
+});
+
+clearButton.addEventListener("click", function () {
+  passengerName.innerText = "";
+  price.innerText = "";
 });
