2d4ce7d0749571d7b524704dca2446a8cbcdbed2
Giovanni-Damico98 <gd.installazioni25@gmail.com>
2024-08-07 23:34:00 +0200

    form progress

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const inputName = document.getElementById("nameComplete");
+const age = document.getElementById("age");
+const button = document.getElementById("calculate");
+
+button.addEventListener("click", function (e) {
+  e.preventDefault();
+  
+});
