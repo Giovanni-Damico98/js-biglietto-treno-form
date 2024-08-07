32d8f7e5306ba27bd68f7f011d4d46d1f0c7c914
Giovanni-Damico98 <gd.installazioni25@gmail.com>
2024-08-08 00:21:43 +0200

    form progress

--- js/script.js
+++ js/script.js
@@ -1,8 +1,21 @@
-const inputName = document.getElementById("nameComplete");
-const age = document.getElementById("age");
+const inputName = document.getElementById("nameField");
+const inputKm = document.getElementById("kmField");
+const inputAge = document.getElementById("ageField");
 const button = document.getElementById("calculate");
+const price = document.getElementById("price");
+const passengerName = document.getElementById("passengerName");
+const priceForKm = 0.21;
 
 button.addEventListener("click", function (e) {
   e.preventDefault();
-  
+  const km = parseFloat(inputKm.value);
+  if (isNaN(km) || km <= 0) {
+    alert("Per favore inserisci un numero valido di chilometri.");
+  } else if (inputName.value === "") {
+    alert("Per favore inserisci un nome valido.");
+  } else {
+    let ticketPrice = inputKm.value * priceForKm;
+    passengerName.innerText = inputName.value.trim();
+    price.innerText = ticketPrice.toFixed(2) + " €";
+  }
 });
